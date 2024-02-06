391ed858f2ac1c98dba69593fd02231a6f0cdda5
Fabbricatore.20111 <Fabbricatore.20111@LABI2-13W-S>
2024-02-06 10:11:14 +0100

    Aggiungere i file di progetto.

---
+++ disegni/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace disegni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(964, 594);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 496);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 589);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ disegni/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace disegni
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public bool disegnaoggetti = true;
+        public int x;
+        public int y;
+        public bool inizio = true;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void DisegnaCerchio(object sender, PaintEventArgs e)
+        {
+
+            if (disegnaoggetti)
+            {
+                if(inizio == true)
+                {
+                    inizio = false;
+
+                    x = 50; y = 50;
+
+                    Graphics g = this.panel1.CreateGraphics();
+                    Pen cerchio = new Pen(Color.Black, 2);
+                    Rectangle rettangolo_cerchio = new Rectangle(x, y, 50, 50);
+
+                    g.DrawEllipse(cerchio, rettangolo_cerchio);
+                }
+                else
+                {
+                    x += 50; y += 50;
+
+                    Graphics g = this.panel1.CreateGraphics();
+                    Pen cerchio = new Pen(Color.Black, 2);
+                    Rectangle rettangolo_cerchio = new Rectangle(x, y, 50, 50);
+
+                    g.DrawEllipse(cerchio, rettangolo_cerchio);
+                }
+            }
+        }
+
+        public void DisegnaLinea(object sender, PaintEventArgs e)
+        {
+            if (disegnaoggetti)
+            {
+                if (inizio == true)
+                {
+                    x = 50; y = 50;
+
+                    Graphics g = this.panel1.CreateGraphics();
+                    Pen linea = new Pen(Color.Black, 2);
+
+                    PointF p1 = new PointF(x, y);
+
+                    x += 50; y += 5;
+
+                    PointF p2 = new PointF(x, y);
+
+                    g.DrawLine(linea, p1, p2);
+                }
+                else
+                {
+                    x += 50; y += 50;
+
+                    Graphics g = this.panel1.CreateGraphics();
+                    Pen linea = new Pen(Color.Black, 2);
+
+                    PointF p1 = new PointF(x, y);
+
+                    x += 50; y += 5;
+
+                    PointF p2 = new PointF(x, y);
+
+                    g.DrawLine(linea, p1, p2);
+                }
+            }
+        }
+
+        public void DisegnaGrafo()
+        {
+            this.Paint += new PaintEventHandler(DisegnaCerchio); 
+            this.Paint += new PaintEventHandler(DisegnaLinea);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string titolo_input = "Numero nodi", frase = "Inserisci il numero di nodi";
+            object input_n = Interaction.InputBox(frase, titolo_input);
+
+            int n = int.Parse(input_n.ToString());
+
+            DisegnaGrafo();
+
+            this.Invalidate();
+
+           
+
+        }
+    }
+}
